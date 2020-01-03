b620780e2173705ca1cd9053dfaa0af394e68b47
Justus Schock <justus.schock@rwth-aachen.de>
2020-01-03 11:31:41 +0100

    Add Centered Trafos

--- rising/transforms/affine.py
+++ rising/transforms/affine.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
 from rising.transforms.abstract import BaseTransform
-from rising.transforms.functional.affine import affine_image_transform
+from rising.transforms.functional.affine import affine_image_transform, \
+    _check_new_img_size
 from rising.utils.affine import AffineParamType, \
     assemble_matrix_if_necessary, matrix_to_homogeneous, matrix_to_cartesian
+from rising.interface import AbstractMixin
 import torch
 from typing import Sequence, Union
 
@@ -583,5 +586,197 @@ class StackedAffine(Affine):
 
         return matrix_to_cartesian(whole_trafo)
 
-# TODO: Add transforms around image center
-# TODO: Add Resize Transform
+
+class CenterShiftMixin(AbstractMixin):
+    def include_shifts(self, matrix, **data) -> torch.Tensor:
+        batchsize = data[self.keys[0]].shape[0]
+        ndim = len(data[self.keys[0]].shape) - 2  # channel and batch dim
+        device = data[self.keys[0]].device
+        dtype = data[self.keys[0]].dtype
+        curr_img_size = data[self.keys[0]].shape[2:]
+
+        shift_to_origin = assemble_matrix_if_necessary(
+            batchsize, ndim, scale=None, rotation=None,
+            translation=tuple([-tmp / 2 for tmp in reversed(curr_img_size)]),
+            matrix=None, degree=False, device=device, dtype=dtype
+        )
+
+        matrix = matrix_to_cartesian(
+            torch.bmm(matrix_to_homogeneous(matrix),
+                      matrix_to_homogeneous(shift_to_origin)))
+        new_img_size = _check_new_img_size(curr_img_size, matrix,
+                                           zero_border=False)
+
+        shift_back_to_center = assemble_matrix_if_necessary(
+            batchsize, ndim,
+            scale=None, rotation=None,
+            translation=tuple([tmp / 2 for tmp in reversed(new_img_size)]),
+            matrix=None, degree=False, device=device, dtype=dtype)
+
+        return matrix_to_cartesian(
+            torch.bmm(
+                matrix_to_homogeneous(shift_back_to_center),
+                matrix_to_homogeneous(matrix)
+            )
+        )
+
+    def assemble_matrix(self, **data) -> torch.Tensor:
+        matrix = super().assemble_matrix(**data)
+        whole_matrix = self.include_shifts(matrix, **data)
+
+        return whole_matrix
+
+
+class RotateAroundCenter(CenterShiftMixin, Rotate):
+    def __init__(self,
+                 rotation: AffineParamType,
+                 keys: Sequence = ('data',),
+                 grad: bool = False,
+                 degree: bool = False,
+                 output_size: tuple = None,
+                 adjust_size: bool = False,
+                 interpolation_mode: str = 'bilinear',
+                 padding_mode: str = 'zeros',
+                 align_corners: bool = False,
+                 **kwargs):
+        """
+        Class Performing a Rotation-OnlyAffine Transformation on a given
+        sample dict.
+        The transformation will be applied to all the dict-entries specified
+        in :attr:`keys`.
+
+        Parameters
+        ----------
+        rotation : torch.Tensor, int, float, optional
+            the rotation factor(s). Supported are:
+                * a full transformation matrix of shape
+                    (BATCHSIZE x NDIM x NDIM)
+                * a single parameter (as float or int), which will be
+                    replicated for all dimensions and batch samples
+                * a single parameter per sample (as a 1d tensor), which will
+                    be replicated for all dimensions
+                * a single parameter per dimension (either as 1d tensor or as
+                    2d transformation matrix), which will be replicated for
+                    all batch samples
+            None will be treated as a rotation factor of 1
+        keys: Sequence
+            keys which should be augmented
+        grad: bool
+            enable gradient computation inside transformation
+        degree : bool
+            whether the given rotation(s) are in degrees.
+            Only valid for rotation parameters, which aren't passed as full
+            transformation matrix.
+        output_size : Iterable
+            if given, this will be the resulting image size.
+            Defaults to ``None``
+        adjust_size : bool
+            if True, the resulting image size will be calculated dynamically
+            to ensure that the whole image fits.
+        interpolation_mode : str
+            interpolation mode to calculate output values
+            'bilinear' | 'nearest'. Default: 'bilinear'
+        padding_mode :
+            padding mode for outside grid values
+            'zeros' | 'border' | 'reflection'. Default: 'zeros'
+        align_corners : Geometrically, we consider the pixels of the input as
+            squares rather than points. If set to True, the extrema (-1 and 1)
+            are considered as referring to the center points of the input’s
+            corner pixels. If set to False, they are instead considered as
+            referring to the corner points of the input’s corner pixels,
+            making the sampling more resolution agnostic.
+        **kwargs :
+                    additional keyword arguments passed to the affine transform
+
+        Notes
+        -----
+        The offsets for shifting back and to origin are calculated on the
+        entry matching the first item iin :attr:`keys` for each batch
+
+        """
+        super().__init__(rotation=rotation,
+                         keys=keys,
+                         grad=grad,
+                         degree=degree,
+                         output_size=output_size,
+                         adjust_size=adjust_size,
+                         interpolation_mode=interpolation_mode,
+                         padding_mode=padding_mode,
+                         align_corners=align_corners,
+                         **kwargs)
+
+
+class ScaleAroundCenter(CenterShiftMixin, Scale):
+    def __init__(self,
+                 scale: AffineParamType,
+                 keys: Sequence = ('data',),
+                 grad: bool = False,
+                 output_size: tuple = None,
+                 adjust_size: bool = False,
+                 interpolation_mode: str = 'bilinear',
+                 padding_mode: str = 'zeros',
+                 align_corners: bool = False,
+                 **kwargs):
+        """
+        Class Performing a Scale-Only Affine Transformation on a given
+        sample dict.
+        The transformation will be applied to all the dict-entries specified
+        in :attr:`keys`.
+
+        Parameters
+        ----------
+        scale : torch.Tensor, int, float, optional
+            the scale factor(s). Supported are:
+                * a full transformation matrix of shape
+                    (BATCHSIZE x NDIM x NDIM)
+                * a single parameter (as float or int), which will be
+                    replicated for all dimensions and batch samples
+                * a single parameter per sample (as a 1d tensor), which will
+                    be replicated for all dimensions
+                * a single parameter per dimension (either as 1d tensor or as
+                    2d transformation matrix), which will be replicated for
+                    all batch samples
+            None will be treated as a scaling factor of 1
+        keys: Sequence
+            keys which should be augmented
+        grad: bool
+            enable gradient computation inside transformation
+        degree : bool
+            whether the given rotation(s) are in degrees.
+            Only valid for rotation parameters, which aren't passed as full
+            transformation matrix.
+        output_size : Iterable
+            if given, this will be the resulting image size.
+            Defaults to ``None``
+        adjust_size : bool
+            if True, the resulting image size will be calculated dynamically
+            to ensure that the whole image fits.
+        interpolation_mode : str
+            interpolation mode to calculate output values
+            'bilinear' | 'nearest'. Default: 'bilinear'
+        padding_mode :
+            padding mode for outside grid values
+            'zeros' | 'border' | 'reflection'. Default: 'zeros'
+        align_corners : Geometrically, we consider the pixels of the input as
+            squares rather than points. If set to True, the extrema (-1 and 1)
+            are considered as referring to the center points of the input’s
+            corner pixels. If set to False, they are instead considered as
+            referring to the corner points of the input’s corner pixels,
+            making the sampling more resolution agnostic.
+        **kwargs :
+            additional keyword arguments passed to the affine transform
+
+        Notes
+        -----
+        The offsets for shifting back and to origin are calculated on the
+        entry matching the first item iin :attr:`keys` for each batch
+        """
+        super().__init__(scale=scale,
+                         keys=keys,
+                         grad=grad,
+                         output_size=output_size,
+                         adjust_size=adjust_size,
+                         interpolation_mode=interpolation_mode,
+                         padding_mode=padding_mode,
+                         align_corners=align_corners,
+                         **kwargs)
