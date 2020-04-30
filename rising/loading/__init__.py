17cf47e419bff54c2987361a7f01053134428029
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-04-30 23:58:08 +0200

    Update __init__.py

--- rising/loading/__init__.py
+++ rising/loading/__init__.py
@@ -1,4 +1,3 @@
 from rising.loading.collate import numpy_collate
-from rising.loading.dataset import Dataset, CacheDataset, LazyDataset
+from rising.loading.dataset import Dataset, AsyncDataset
 from rising.loading.loader import DataLoader, default_transform_call
-from rising.loading.debug_mode import get_debug_mode, set_debug_mode, switch_debug_mode
