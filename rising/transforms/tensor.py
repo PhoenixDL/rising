dc33086ab78d3f749baeac610dc313770afdde41
Justus Schock <justus.schock@rwth-aachen.de>
2020-05-01 14:30:15 +0200

    google docstrings for tensor ops

--- rising/transforms/tensor.py
+++ rising/transforms/tensor.py
@@ -10,86 +10,60 @@ __all__ = ["ToTensor", "ToDevice", "TensorOp", "Permute"]
 
 
 class ToTensor(BaseTransform):
+    """Transform Input Collection to Collection of :class:`torch.Tensor`"""
     def __init__(self, keys: Sequence = ('data',), grad: bool = False, **kwargs):
         """
-        Apply augment_fn to keys
-
-        Parameters
-        ----------
-        augment_fn: callable
-            function for augmentation
-        keys: Sequence
-            keys which should be augmented
-        grad: bool
-            enable gradient computation inside transformation
-        kwargs:
-            keyword arguments passed to augment_fn
+        Args:
+            keys: keys which should be transformed
+        grad: enable gradient computation inside transformation
+        **kwargs: keyword arguments passed to augment_fn
         """
         super().__init__(augment_fn=default_convert, keys=keys, grad=grad, **kwargs)
 
 
 class ToDevice(BaseTransform):
+    """Push data to device"""
     def __init__(self, device: Union[torch.device, str],
                  non_blocking: bool = False, copy: bool = False,
                  keys: Sequence = ('data',), grad: bool = False, **kwargs):
         """
-        Push data to device
-
-        Parameters
-        ----------
-        device: Union[torch.device, str]
-            target device
-        non_blocking: bool
-            if True and this copy is between CPU and GPU, the copy may occur asynchronously
-            with respect to the host. For other cases, this argument has no effect.
-        copy: bool
-            create copy of data
-        keys: Sequence
-            keys which should be augmented
-        grad: bool
-            enable gradient computation inside transformation
-        kwargs:
-            keyword arguments passed to function
+        Args:
+            device: target device
+            non_blocking: if True and this copy is between CPU and GPU,
+                the copy may occur asynchronously with respect to the host.
+                For other cases, this argument has no effect.
+            copy: create copy of data
+            keys: keys which should be augmented
+            grad: enable gradient computation inside transformation
+            **kwargs: keyword arguments passed to function
         """
         super().__init__(augment_fn=to_device, keys=keys, grad=grad, device=device,
                          non_blocking=non_blocking, copy=copy, **kwargs)
 
 
 class TensorOp(BaseTransform):
+    """Apply function which are supported by the `torch.Tensor` class"""
     def __init__(self, op_name: str, *args, keys: Sequence = ('data',),
                  grad: bool = False, **kwargs):
         """
-        Apply function which are supported by the `torch.Tensor` class
-
-        Parameters
-        ----------
-        op_name: str
-            name of tensor operation
-        args:
-            positional arguments passed to function
-        keys: Sequence
-            keys which should be augmented
-        grad: bool
-            enable gradient computation inside transformation
-        kwargs:
-            keyword arguments passed to function
+        Args:
+            op_name: name of tensor operation
+            *args: positional arguments passed to function
+            keys: keys which should be augmented
+            grad: enable gradient computation inside transformation
+            **kwargs: keyword arguments passed to function
         """
         super().__init__(tensor_op, op_name, *args, keys=keys, grad=grad, **kwargs)
 
 
-class Permute(AbstractTransform):
+class Permute(BaseTransform):
+    """Permute dimensions of tensor"""
     def __init__(self, dims: Dict[str, Sequence[int]], grad: bool = False, **kwargs):
         """
-        Permute dimensions of tensor
-
-        Parameters
-        ----------
-        dims: Dict[str, Sequence[int]]
-            defines permutation sequence for respective key
-        grad: bool
-            enable gradient computation inside transformation
-        kwargs:
-            keyword arguments passed to permute function
+        Args:
+            dims: defines permutation sequence for respective key
+            grad: enable gradient computation inside transformation
+            **kwargs: keyword arguments passed to permute function
         """
         super().__init__(grad=grad)
         self.dims = dims
@@ -99,14 +73,10 @@ class Permute(AbstractTransform):
         """
         Forward input
 
-        Parameters
-        ----------
-        data: dict
-            batch dict
+        Args:
+        data: batch dict
 
-        Returns
-        -------
-        dict
+        Returns:
             augmented data
         """
         for key, item in self.dims.items():
