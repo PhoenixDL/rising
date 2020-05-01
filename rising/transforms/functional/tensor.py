07749ce82693fff845e27650d95d64163df25aab
Justus Schock <justus.schock@rwth-aachen.de>
2020-05-01 11:53:56 +0200

    google docstrings for functional tensor ops

--- rising/transforms/functional/tensor.py
+++ rising/transforms/functional/tensor.py
@@ -11,28 +11,23 @@ def tensor_op(data: data_type, fn: str, *args, **kwargs) -> data_type:
     """
     Invokes a function form a tensor
 
-    Parameters
-    ----------
-    data: data_type
-        data which should be pushed to device. Sequence and mapping items are
-        mapping individually to gpu
-    fn: str
-        tensor function
-    args:
-        positional arguments passed to tensor function
-    kwargs:
-        keyword arguments passed to tensor function
+    Args:
+    data: data which should be pushed to device. Sequence and mapping items
+        are mapping individually to gpu
+    fn: tensor function
+    *args: positional arguments passed to tensor function
+    **kwargs: keyword arguments passed to tensor function
 
-    Returns
-    -------
-    data_type:
+    Returns:
         data which was pushed to device
     """
     if torch.is_tensor(data):
         return getattr(data, fn)(*args, **kwargs)
     elif isinstance(data, Mapping):
-        return {key: tensor_op(item, fn, *args, **kwargs) for key, item in data.items()}
+        return {key: tensor_op(item, fn, *args, **kwargs)
+                for key, item in data.items()}
     elif isinstance(data, (list, tuple)):
-        return type(data)([tensor_op(item, fn, *args, **kwargs) for item in data])
+        return type(data)([tensor_op(item, fn, *args, **kwargs)
+                           for item in data])
     else:
         return data
