e0d03b2b475bbda98a89b1c9c26f94c539ae483b
Justus Schock <justus.schock@rwth-aachen.de>
2020-05-01 13:12:22 +0200

    google docstrings for compose

--- rising/transforms/compose.py
+++ rising/transforms/compose.py
@@ -1,11 +1,10 @@
-import torch
 from random import shuffle
 from typing import Sequence, Union, Callable, Any, Mapping
 
+import torch
 
-from rising.utils import check_scalar
 from rising.transforms import AbstractTransform, RandomProcess
-
+from rising.utils import check_scalar
 
 __all__ = ["Compose", "DropoutCompose"]
 
@@ -14,34 +13,29 @@ def dict_call(batch: dict, transform: Callable) -> Any:
     """
     Unpacks the dict for every transformation
 
-    Parameters
-    ----------
-    batch: dict
-        current batch which is passed to transform
-    transform: Callable
-        transform to perform
+    Args:
+        batch: current batch which is passed to transform
+        transform: transform to perform
 
-    Returns
-    -------
-    Any
+    Returns:
         transformed batch
     """
     return transform(**batch)
 
 
 class _TransformWrapper(torch.nn.Module):
+    """
+    Helper Class to wrap all non-module transforms into modules to use the
+    torch.nn.ModuleList as container for the transforms. This enables
+    forwarding of all model specific calls as ``.to()`` to all transforms
+    """
+
     def __init__(self, trafo: Callable):
         """
-        Helper Class to wrap all non-module transforms into modules to use the
-        torch.nn.ModuleList as container for the transforms. This enables
-        forwarding of all model specific calls as ``.to()`` to all transforms
-
-        Parameters
-        ----------
-        trafo : Callable
-            the actual transform, which will be wrapped by this class.
-            Since this transform is no subclass of ``torch.nn.Module``,
-            its internal state won't be affected by module specific calls
+        Args:
+            trafo: the actual transform, which will be wrapped by this class.
+                Since this transform is no subclass of ``torch.nn.Module``,
+                its internal state won't be affected by module specific calls
         """
         super().__init__()
 
@@ -55,21 +49,20 @@ class _TransformWrapper(torch.nn.Module):
 
 
 class Compose(AbstractTransform):
+    """
+    Compose multiple transforms
+    """
+
     def __init__(self, *transforms, shuffle: bool = False,
                  transform_call: Callable[[Any, Callable], Any] = dict_call):
         """
-        Compose multiple transforms
-
-        Parameters
-        ----------
-        transforms: Union[AbstractTransform, Sequence[AbstractTransform]]
-            one or multiple transformations which are applied in consecutive
-            order
-        shuffle: bool
-            apply transforms in random order
-        transform_call: Callable[[Any, Callable], Any], optional
-            function which determines how transforms are called. By default
-            Mappings and Sequences are unpacked during the transform.
+        Args
+            transforms: one or multiple transformations which are applied
+                in consecutive order
+            shuffle: apply transforms in random order
+            transform_call: function which determines how transforms are
+                called. By default Mappings and Sequences are unpacked
+                during the transform.
         """
         super().__init__(grad=True)
         if isinstance(transforms[0], Sequence):
@@ -84,17 +77,12 @@ class Compose(AbstractTransform):
         Apply transforms in a consecutive order. Can either handle
         Sequence like or Mapping like data.
 
-        Parameters
-        ----------
-        seq_like: tuple
-            data which is unpacked like a Sequence
-        map_like: dict
-            data which is unpacked like a dict
+        Args:
+            *seq_like: data which is unpacked like a Sequence
+            **map_like: data which is unpacked like a dict
 
-        Returns
-        -------
-        dict
-            dict with transformed data
+        Returns:
+            transformed data
         """
         assert not (seq_like and map_like)
         assert len(self.transforms) == len(self.transform_order)
@@ -112,9 +100,7 @@ class Compose(AbstractTransform):
         """
         Transforms getter
 
-        Returns
-        -------
-        torch.nn.ModuleList
+        Returns:
             transforms to compose
         """
         return self._transforms
@@ -125,15 +111,11 @@ class Compose(AbstractTransform):
         """
         Transforms setter
 
-        Parameters
-        ----------
-        transforms: Union[AbstractTransform, Sequence[AbstractTransform]]
-            one or multiple transformations which are applied in consecutive
-            order
+        Args:
+        transforms: one or multiple transformations which are applied in
+            consecutive order
 
-        Returns
-        -------
-        torch.nn.ModuleList
+        Returns:
             transforms to compose
         """
         # make transforms a list to be mutable.
@@ -153,9 +135,7 @@ class Compose(AbstractTransform):
         """
         Getter for attribute shuffle
 
-        Returns
-        -------
-        bool
+        Returns:
             True if shuffle is enabled, False otherwise
         """
         return self._shuffle
@@ -165,45 +145,43 @@ class Compose(AbstractTransform):
         """
         Setter for shuffle
 
-        Parameters
-        ----------
-        shuffle : bool
-            new status of shuffle
+        Args:
+            shuffle : new status of shuffle
         """
         self._shuffle = shuffle
         self.transform_order = list(range(len(self.transforms)))
 
 
 class DropoutCompose(RandomProcess, Compose):
+    """
+    Compose multiple transforms to one and randomly apply them
+    """
+
     def __init__(self, *transforms, dropout: Union[float, Sequence[float]] = 0.5,
-                 random_mode: str = "random", random_args: Sequence = (), random_module: str = "random",
+                 random_mode: str = "random", random_args: Sequence = (),
+                 random_module: str = "random",
                  transform_call: Callable[[Any, Callable], Any] = dict_call,
                  **kwargs):
         """
-        Compose multiple transforms to one
+        Args:
+            *transforms: one or multiple transformations which are applied in c
+                onsecutive order
+            dropout: if provided as float, each transform is skipped with the
+                given probability
+                if :param:`dropout` is a sequence, it needs to specify the
+                dropout probability for each given transform
+            random_mode: specifies distribution which should be used to sample
+                additive value
+            random_args: positional arguments passed for random function
+            random_module: module from where function random function should
+                be imported
+            transform_call: function which determines how transforms are
+                called. By default Mappings and Sequences are unpacked
+                during the transform.
 
-        Parameters
-        ----------
-        transforms: Union[AbstractTransform, Sequence[AbstractTransform]]
-            one or multiple transformations which are applied in consecutive order
-        dropout: Union[float, Sequence[float]]
-            if provided as float, each transform is skipped with the given probability
-            if :param:`dropout` is a sequence, it needs to specify the dropout
-            probability for each given transform
-        random_mode: str
-            specifies distribution which should be used to sample additive value
-        random_args: Sequence
-            positional arguments passed for random function
-        random_module: str
-            module from where function random function should be imported
-        transform_call: Callable[[Any, Callable], Any], optional
-            function which determines how transforms are called. By default
-            Mappings and Sequences are unpacked during the transform.
-
-        Raises
-        ------
-        TypeError
-            if dropout is a sequence it must have the same length as transforms
+        Raises:
+            ValueError: if dropout is a sequence it must have the same length
+                as transforms
         """
         super().__init__(*transforms, random_mode=random_mode,
                          random_args=random_args, random_module=random_module,
@@ -211,9 +189,9 @@ class DropoutCompose(RandomProcess, Compose):
         if check_scalar(dropout):
             dropout = [dropout] * len(self.transforms)
         if len(dropout) != len(self.transforms):
-            raise TypeError(f"If dropout is a sequence it must specify the dropout probability "
-                            f"for each transform, found {len(dropout)} probabilities "
-                            f"and {len(self.transforms)} transforms.")
+            raise ValueError(f"If dropout is a sequence it must specify the dropout probability "
+                             f"for each transform, found {len(dropout)} probabilities "
+                             f"and {len(self.transforms)} transforms.")
         self.dropout = dropout
 
     def forward(self, *seq_like, **map_like) -> Union[Sequence, Mapping]:
@@ -221,16 +199,11 @@ class DropoutCompose(RandomProcess, Compose):
         Apply transforms in a consecutive order. Can either handle
         Sequence like or Mapping like data.
 
-        Parameters
-        ----------
-        seq_like: tuple
-            data which is unpacked like a Sequence
-        map_like: dict
-            data which is unpacked like a dict
+        Args:
+            *seq_like: data which is unpacked like a Sequence
+            **map_like: data which is unpacked like a dict
 
-        Returns
-        -------
-        dict
+        Returns:
             dict with transformed data
         """
         assert not (seq_like and map_like)
