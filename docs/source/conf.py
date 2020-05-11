9c3bff8e4520f2e18694e74370cfe03b61890c15
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-05-11 11:53:47 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,7 +111,7 @@ exclude_patterns = [
 ]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'python3'
 
 # -- Options for HTML output -------------------------------------------------
 
