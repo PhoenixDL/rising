aebc852bf4bed1c0ccc9c39c0a6dc1cbacfb16ef
Michael Baumgartner <m.baumgartner@ymail.com>
2020-05-15 18:28:43 +0000

    autopep8 fix

--- rising/random/__init__.py
+++ rising/random/__init__.py
@@ -1,5 +1,5 @@
 from rising.random.abstract import AbstractParameter
-from rising.random.continuous import (ContinuousParameter, NormalParameter, 
-                                                                   UniformParameter)
-    UniformParameter
+from rising.random.continuous import (ContinuousParameter, NormalParameter,
+                                      UniformParameter)
+UniformParameter
 from rising.random.discrete import DiscreteParameter
