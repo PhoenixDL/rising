7e7708a234b6e4a5e80760aab2fa258cec668699
Michael Baumgartner <m.baumgartner@ymail.com>
2020-05-15 18:30:18 +0000

    autopep8 fix

--- rising/utils/checktype.py
+++ rising/utils/checktype.py
@@ -12,7 +12,7 @@ def check_scalar(x):
         True if input is scalar
     """
     return isinstance(x, (int, float)) or (is instance(x, torch.Tensor) and x.numel() == 1)
-        return True
+    return True
     elif torch.is_tensor(x):
         return x.numel() == 1
     else:
