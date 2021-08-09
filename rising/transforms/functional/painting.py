e8b0f828fe0d7babbe3d4f9d20ee91e776944869
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-08-09 10:53:34 +0200

    Update rising/transforms/functional/painting.py
    Co-authored-by: Paul <paul.weinmann@rwth-aachen.de>

--- rising/transforms/functional/painting.py
+++ rising/transforms/functional/painting.py
@@ -3,7 +3,7 @@ import torch
 __all__ = ["local_pixel_shuffle", "random_inpainting", "random_outpainting"]
 
 
-def local_pixel_shuffle(data: torch.Tensor, n: int = -1) -> torch.Tensor:
+def local_pixel_shuffle(data: torch.Tensor, n: int = -1, block_size: tuple=(0,0,0), rel_block_size: float = 0.1) -> torch.Tensor:
 
     batch_size, channels, img_rows, img_cols, img_deps = data.size()
 
@@ -12,9 +12,14 @@ def local_pixel_shuffle(data: torch.Tensor, n: int = -1) -> torch.Tensor:
     for b in range(batch_size):
         for _ in range(n):
             c = torch.randint(0,channels-1, (1,))
-            block_size_x = torch.randint(2, img_rows // 10, (1,))
-            block_size_y = torch.randint(2, img_cols // 10, (1,))
-            block_size_z = torch.randint(2, img_deps // 10, (1,))
+            
+            (block_size_x, block_size_y, block_size_z)  = (torch.tensor([size]) for size in block_size)
+            
+            if rel_block_size > 0:
+                block_size_x = torch.randint(2, int(img_rows * rel_block_size), (1,))
+                block_size_y = torch.randint(2, int(img_cols * rel_block_size), (1,))
+                block_size_z = torch.randint(2, int(img_deps * rel_block_size), (1,))
+
             x = torch.randint(0, int(img_rows - block_size_x), (1,))
             y = torch.randint(0, int(img_cols - block_size_y), (1,))
             z = torch.randint(0, int(img_deps - block_size_z), (1,))
